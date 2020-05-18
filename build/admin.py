e72e63f4472f038a24778041af8525c3791da5fb
ZakariaTalhami <undeadqbs@gmail.com>
2020-05-18 10:27:48 +0300

    Revert "fetch builds, store in db, paginitions"

--- build/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from build.models import Build
-
-admin.site.register(Build)
--- build/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Build
-
-
-class BuildSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Build
-        fields = "__all__"
-        depth = 2
